10556f59e513a45741c0cc06cfa1b5fe52196b09
lilactree2 <118321591+lilactree2@users.noreply.github.com>
2025-12-08 16:46:47 +0900

    Fix module import path for CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,9 @@
-from fastapi.testclient import TestClient
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
